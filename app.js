49a6308c2067693654dc73e361534e073d486f3c
Jordanpb <jb0102@uah.edu>
2016-06-15 16:56:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var main = funtion(){
   $('.page-heading').click(funtion(){
-    $('#dropdown-menu').show('dd1');  
+    $('#dropdown-menu').show('dd1');
+    $(document).fadIn(100);
   });
 };
 $(document).ready(main);
