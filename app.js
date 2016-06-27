59522e91ffce18f136422affedb3a6a366a9061e
Jordanpb <jb0102@uah.edu>
2016-06-27 11:20:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var main = funtion(){
+/*var main = funtion(){
 $('body').hide();
 $('nav li ul').hover().removeClass('fallback');
 $('nav li').hover(
@@ -10,3 +10,15 @@ $('nav li').hover(
   }
 };
 $(document).ready(main);
+*/
+
+$(document).ready(function(){
+  $('body').hide();
+  $('nave li').click(function(){
+    $('ul', this).stop().slideDown('slow');
+  },
+    function(){
+    $('ul', this).stop().slideUp('slow');
+  });
+  
+});
